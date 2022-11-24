c0c3bd885c1c3b4a5b56991da1de1c76e1210e7e
soniatripano <116813883+soniatripano@users.noreply.github.com>
2022-11-24 16:23:41 +0100

    Update queries.sh

--- queries.sh
+++ queries.sh
@@ -29,10 +29,10 @@ echo -e "\nWinner of the 2018 tournament team name:"
 echo  "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE year=2018 AND round='Final'")"
 
 echo -e "\nList of teams who played in the 2014 'Eighth-Final' round:"
-echo  "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE year=2014 AND round='Eight-Final'")"
+echo  "$($PSQL "SELECT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id AND teams.team_id=games.opponent_id WHERE year=2014 AND round='Eight-Final'")"
 
 echo -e "\nList of unique winning team names in the whole data set:"
-echo  "$($PSQL "SELECT DISTINCT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE round='Final'")"
+echo  "$($PSQL "SELECT DISTINCT name FROM teams INNER JOIN games ON teams.team_id=games.winner_id ORDER BY name ASC")"
 
 echo -e "\nYear and team name of all the champions:"
 echo  "$($PSQL "SELECT year, name FROM teams INNER JOIN games ON teams.team_id=games.winner_id WHERE round='Final'")"
